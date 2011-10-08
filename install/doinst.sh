74a51fe77f565f8d24851a15eb6983e0f860a84e
Andrei G <oz@ozlaptop.oprod.net>
2011-10-08 20:42:48 +0300

    modified:   install/doinst.sh 	deleted:    install/slack-desc~

--- install/doinst.sh
+++ install/doinst.sh
@@ -1,18 +1,3 @@
-config() {
-  NEW="$1"
-  OLD="$(dirname $NEW)/$(basename $NEW .new)"
-  # If there's no config file by that name, mv it over:
-  if [ ! -r $OLD ]; then
-    mv $NEW $OLD
-  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
-    # toss the redundant copy
-    rm $NEW
-  fi
-  # Otherwise, we leave the .new copy for the admin to consider...
-}
-
-config /etc/rc.d/rc.zram.new
-
 echo "# Start zram swap space" >> /etc/rc.d/rc.local
 echo "if [ -x /etc/rc.d/rc.zram ]" >> /etc/rc.d/rc.local
 echo "then" >> /etc/rc.d/rc.local
